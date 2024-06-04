56e8d70fa55de4e65c0b5c162e91a12bcf07b87a
DenGrif <grifon991@gmail.com>
2024-06-04 08:36:06 +0400

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,14 @@
+my_string = input("Введите ваше имя: ")
+print(my_string.upper())
+my_string = input("Введите ваше имя: ")
+print(my_string.lower())
+my_string = "Введите ваше имя: ".upper()
+print(my_string)
+my_string = "Введите ваше имя: ".lower()
+print(my_string)
+my_string = "Введите ваше имя: ".replace(' ','')
+print(my_string)
+my_string = "Введите ваше имя: " [0]
+print(my_string)
+my_string = "Введите ваше имя:" [-1]
+print(my_string)
